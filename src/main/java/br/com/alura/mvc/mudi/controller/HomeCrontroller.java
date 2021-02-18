c25d55473dc7d26ef3993c94151bf4c138622901
Marcus Quintela <marcus.quintela@gmail.com>
2021-02-18 10:10:33 -0300

    Criando primeira tela dom Thymeleaf

---
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeCrontroller.java
@@ -0,0 +1,24 @@
+package br.com.alura.mvc.mudi.controller;
+
+import br.com.alura.mvc.mudi.model.Pedido;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class HomeCrontroller {
+
+    @GetMapping("/home")
+    public String home(Model model){
+        Pedido pedido = Pedido.build();
+
+        List<Pedido> pedidos = Arrays.asList(pedido);
+
+        model.addAttribute("pedidos", pedidos);
+
+        return "home";
+    }
+}
---
+++ src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -0,0 +1,26 @@
+package br.com.alura.mvc.mudi.model;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+public class Pedido {
+    private String nomeProduto;
+    private BigDecimal valorNegociado;
+    private LocalDate dataEntrega;
+    private String urlProduto;
+    private String urlImagem;
+    private String descricao;
+
+    public static Pedido build() {
+        Pedido pedido = new Pedido();
+        pedido.setNomeProduto("Celular Xiaomi Note 8 64GB Rom 4GB Ram Dual Versión Global Space Black");
+        pedido.setUrlImagem("https://images-na.ssl-images-amazon.com/images/I/51wgmCYDFML._AC_SL1000_.jpg");
+        pedido.setUrlProduto("https://www.amazon.com.br/Celular-Xiaomi-Vers%C3%A3o-Global-Space/dp/B07Y9ZHLXW/ref=sr_1_2?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=Xiaomi+Redmi+Note+8&qid=1613609175&sr=8-2");
+        pedido.setDescricao("Descrição qualquer do pedido");
+
+        return pedido;
+    }
+}
