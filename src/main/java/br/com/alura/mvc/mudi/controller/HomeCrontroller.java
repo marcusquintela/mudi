43d1a43a52510ed801a8180a504e0c6d6ef0f76a
marcusquintela <2017Abcd!>
2021-02-18 17:23:26 -0300

    incluindo acesso a banco via repositorio

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeCrontroller.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeCrontroller.java
@@ -1,22 +1,24 @@
 package br.com.alura.mvc.mudi.controller;
 
 import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class HomeCrontroller {
 
+    @Autowired
+    PedidoRepository pedidoRepository;
+
     @GetMapping("/home")
-    public String home(Model model){
-        Pedido pedido = Pedido.build();
+    public String home(Model model) {
 
-        List<Pedido> pedidos = Arrays.asList(pedido, pedido, pedido, pedido);
-
+        List<Pedido> pedidos = pedidoRepository.findAll();
         model.addAttribute("pedidos", pedidos);
 
         return "home";
